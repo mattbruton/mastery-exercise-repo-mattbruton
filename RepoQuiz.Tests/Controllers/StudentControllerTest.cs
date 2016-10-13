fc5417cdd54751e65eec0a82b4c08a3711abdf26
MATT BRUTON <the.bruton@gmail.com>
2016-10-12 22:47:44 -0500

    looked through files supplied. created view pages for students

---
+++ RepoQuiz.Tests/Controllers/StudentControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RepoQuiz.Tests.Controllers
+{
+    [TestClass]
+    public class StudentControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
